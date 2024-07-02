45a1938ca98327513e538d10aa756ee9f982816e
manu <100472310@alumnos.uc3m.es>
2024-07-02 18:08:30 +0200

    cambio en link de linkedin

--- src/components/MyFooter.tsx
+++ src/components/MyFooter.tsx
@@ -11,7 +11,7 @@ const MyFooter = () => {
         <div className="grid grid-cols-3 place-items-center md:w-1/4 w-full">
             <Footer.Title title="Contacto" className="col-span-3"/>
             <Footer.Icon href="https://mail.google.com/mail/?view=cm&fs=1&to=manuelgpr10@gmail.com" icon={MdOutlineEmail} className="text-red-500"/>
-            <Footer.Icon href="www.linkedin.com/in/manuelgpr" icon={BsLinkedin} className="text-blue-500"/>
+            <Footer.Icon href="https://www.linkedin.com/in/manuelgpr" icon={BsLinkedin} className="text-blue-500"/>
             <Footer.Icon href="https://github.com/ManuGPR/" icon={BsGithub} />
         </div>
       </Footer>
