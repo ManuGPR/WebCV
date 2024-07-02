bf0b73e9242fcbe425a064b93869ea22d9367632
manu <100472310@alumnos.uc3m.es>
2024-07-02 20:42:33 +0200

    cambio botones

--- src/components/Curriculum.tsx
+++ src/components/Curriculum.tsx
@@ -9,12 +9,12 @@ const Curriculum = () => {
       <div className='grid grid-cols-1 place-items-center w-screen m-auto'>
         <Button onClick={() => {window.open("/CV.pdf", '_blank');}} className='bg-blue-500 md:w-1/3 w-4/5 py-2 mb-2'>
           <p className='text-xl'>
-            Ver Curriculum en Español
+            Curriculum en Español
           </p>
         </Button>
         <Button onClick={() => {window.open("/CV_ENG.pdf", '_blank');}} className='bg-blue-500 md:w-1/3 w-4/5 py-2'>
           <p className='text-xl'>
-            Ver Curriculum en Inglés
+            CV in English
           </p>
         </Button>
       </div>
