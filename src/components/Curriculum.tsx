6e5974d413c27799abf3b705e7a98ac1462a4635
manu <100472310@alumnos.uc3m.es>
2024-07-03 21:32:49 +0200

    cambio tonto

--- src/components/Curriculum.tsx
+++ src/components/Curriculum.tsx
@@ -9,7 +9,7 @@ const Curriculum = () => {
       <div className='grid grid-cols-1 place-items-center w-screen m-auto'>
         <Button onClick={() => {window.open("/CV.pdf", '_blank');}} className='bg-blue-500 md:w-1/3 w-4/5 py-2 mb-2'>
           <p className='text-xl'>
-            Curriculum en Español
+            CV en Español
           </p>
         </Button>
         <Button onClick={() => {window.open("/CV_ENG.pdf", '_blank');}} className='bg-blue-500 md:w-1/3 w-4/5 py-2'>
